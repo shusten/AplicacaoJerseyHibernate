1c7975b7ed76b00f4a63d929592318471f8f0daa
Luis Villas <shusten@hotmail.com>
2019-12-19 08:45:42 -0300

    Finalizada camada de serviço

---
+++ src/main/java/br/com/cov/webservice/service/ProdutoService.java
@@ -0,0 +1,38 @@
+package br.com.cov.webservice.service;
+
+import br.com.cov.webservice.model.dao.ProdutoDAO;
+import br.com.cov.webservice.model.domain.Produto;
+
+import java.util.List;
+
+public class ProdutoService {
+    private ProdutoDAO dao = new ProdutoDAO();
+
+    public List<Produto> getProdutos() {
+        return dao.getAll();
+    }
+
+    public Produto getProdutos(Long id) {
+        return dao.getById(id);
+    }
+
+    public Produto saveProduto(Produto produto) {
+        return dao.save(produto);
+    }
+
+    public Produto updateProduro(Produto produto) {
+        return dao.update(produto);
+    }
+
+    public Produto deleteProduto(Long id) {
+        return dao.delete(id);
+    }
+
+    public List<Produto> getProdutosByPagination(int firstResult, int maxResults) {
+        return dao.getByPagination(firstResult, maxResults);
+    }
+
+    public List<Produto> getProdutoByName(String name) {
+        return dao.getByName(name);
+    }
+}
