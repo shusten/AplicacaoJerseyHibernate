c7fd8182d35ce6f150d12e5bb8c7bad13b7e3d41
Luis Villas <shusten@hotmail.com>
2019-12-19 02:25:56 -0300

    Add classe ProdutoDAO

---
+++ src/main/java/br/com/cov/webservice/model/dao/ProdutoDAO.java
@@ -0,0 +1,82 @@
+package br.com.cov.webservice.model.dao;
+
+import br.com.cov.webservice.model.domain.Produto;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class ProdutoDAO {
+    public List<Produto> getAll() {
+        EntityManager em = JPAUtil.getEntityManager();
+        List<Produto> produtos = null;
+
+        try {
+            produtos = em.createQuery("select p from Produto p", Produto.class).getResultList();
+        } finally {
+            em.close();
+        }
+
+        return produtos;
+    }
+
+    public Produto getById(long id) {
+        EntityManager em = JPAUtil.getEntityManager();
+        Produto produto = null;
+
+        try {
+            produto = em.find(Produto.class, id);
+        } finally {
+            em.close();
+        }
+        return produto;
+    }
+
+    public Produto save(Produto produto) {
+        EntityManager em = JPAUtil.getEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.persist(produto);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+        return produto;
+    }
+
+    public Produto update(Produto produto) {
+        EntityManager em = JPAUtil.getEntityManager();
+        Produto produtoManaged = null;
+
+        try {
+            em.getTransaction().begin();
+            produtoManaged = em.find(Produto.class, produto.getId());
+            produtoManaged.setNome(produto.getNome());
+            produtoManaged.setQuantidade(produto.getQuantidade());
+            //em.merge(produtoManaged);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+
+        return produtoManaged;
+    }
+
+    public Produto delete(Long id) {
+        EntityManager em = JPAUtil.getEntityManager();
+        Produto produto = null;
+
+        try {
+            em.getTransaction().begin();
+            produto = em.find(Produto.class, id);
+            em.remove(produto);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+
+        return produto;
+    }
+
+
+}
